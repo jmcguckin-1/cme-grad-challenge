e651474823cdb3c238add6fe8da3eb8c2b1a5687
John McGuckin <johnmcguckin@Johns-MacBook.local>
2024-07-21 13:09:17 +0100

    file manipulation complete, challenge ready for review

--- code/src/main/java/org/example/FileManipulation.java
+++ code/src/main/java/org/example/FileManipulation.java
@@ -4,26 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileManipulation {
     private String filename;
     private BufferedReader br;
+    private BufferedReader br1;
     public FileManipulation(String filename) throws FileNotFoundException {
         this.filename = filename;
         br = new BufferedReader(new FileReader(filename));
+        br1 = new BufferedReader(new FileReader(filename));
     }
 
     public boolean insertUnique(String s){
         try {
             String line = br.readLine();
             while (line != null) {
-                System.out.println("Line:" + line + ", " + "S:" + s);
                 if (line.equals(s)){
                     return false;
                 }
                 line = br.readLine();
             }
-
             br.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -31,7 +32,16 @@ public class FileManipulation {
         return true;
     }
 
-    public void loadResults(){
-
+    public void loadResults(ArrayList<String> a){
+        try {
+            String line = br1.readLine();
+            while (line != null) {
+                a.add(line);
+                line = br1.readLine();
+            }
+            br1.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- code/src/main/java/org/example/Main.java
+++ code/src/main/java/org/example/Main.java
@@ -21,11 +21,28 @@ public class Main {
         System.out.println(findPrimeNumbers(sequence));
     }
 
+    /**
+     * Finds prime numbers in a string sequence
+     * @param sequence - the sequence in question
+     * @return a string with the prime numbers present in the sequence.
+     * @throws Exception - in case of invalid input.
+     */
     public static String findPrimeNumbers(String sequence) throws Exception {
+        File f = new File("primenumbers.txt");
+        boolean numbersPresent = true;
         ArrayList<String> finalNumbers = new ArrayList<>();
-        Pattern my_pattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
-        Matcher my_match = my_pattern.matcher(sequence);
-        boolean check = my_match.find();
+        if (f.exists()){
+            // loads previous files results
+            FileManipulation fm = new FileManipulation("primenumbers.txt");
+            fm.loadResults(finalNumbers);
+        }
+        // updated cache array in case of new sequence.
+        ArrayList<String> updatedCache = new ArrayList<>();
+
+        Pattern myPattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher myMatch = myPattern.matcher(sequence);
+        boolean check = myMatch.find();
+        // validation for letters, special characters and blank spaces
         if (check){
             Logs.log(Level.WARNING, "Incorrect sequence - special characters included");
             throw new Exception("Sequence should not contain special characters");
@@ -39,45 +56,61 @@ public class Main {
             throw new Exception("Sequence should not contain blank spaces");
         }
         else{
-            File f = new File("primenumbers.txt");
+            // creates the prime numbers file.
             FileWriter fw = new FileWriter("primenumbers.txt", true);
             BufferedWriter bw = new BufferedWriter(fw);
             for (int i = 0; i < sequence.length(); i++) {
                 for (int j = 0; j < sequence.length() + 1; j++) {
                     if (i < j){
+                        // grabs the strings in sequence.
                         String substr = sequence.substring(i, j);
                         int currentPrime = Integer.parseInt(substr);
-                        if (isPrime(currentPrime)){
-                            finalNumbers.add(substr);
-                            Logs.log(Level.INFO, "Prime Number Found and Added to Cache");
-                            if (f.createNewFile()){
-                                bw.write(substr);
-                                bw.newLine();
-                                Logs.log(Level.INFO, "File Created for First Time");
-                            }
-                            else{
-                                FileManipulation fm = new FileManipulation("primenumbers.txt");
-                                if (fm.insertUnique(substr)){
+                        if (!finalNumbers.contains(substr)){
+                            numbersPresent = false;
+                            if (isPrime(currentPrime)){
+                                finalNumbers.add(substr); // adds to cache
+                                updatedCache.add(substr);
+                                Logs.log(Level.INFO, "Prime Number Found and Added to Cache");
+                                if (f.createNewFile()){
                                     bw.write(substr);
                                     bw.newLine();
+                                    Logs.log(Level.INFO, "File Created for First Time");
                                 }
-                                Logs.log(Level.INFO, "Appending numbers to file");
+                                else{
+                                    FileManipulation fm = new FileManipulation("primenumbers.txt");
+                                    // writes unique values to file
+                                    if (fm.insertUnique(substr) && f.exists()){
+                                        bw.write(substr);
+                                        bw.newLine();
+                                    }
+                                    Logs.log(Level.INFO, "Appending numbers to file");
+                                }
                             }
                         }
+                        else if (sequence.contains(substr)){
+                            updatedCache.add(substr);
+                        }
                     }
-
                 }
             }
             bw.close();
         }
         String message = "Prime Numbers in sequence: ";
-        for (int i=0; i<finalNumbers.size()-1; i++){
-            message += finalNumbers.get(i) + ",";
+        // deduces which arraylist to use, the pre-populated one or the new one
+        ArrayList<String> finalList =  (numbersPresent) ? finalNumbers : updatedCache;
+        for (int i=0; i<finalList.size()-1; i++){
+            message += finalList.get(i) + ",";
         }
-        message += finalNumbers.getLast();
+        message += finalList.getLast();
+
         return message;
     }
 
+    /**
+     * deduces whether or not a number is prime
+     * @param a - the number in question
+     * @return true or false based on a number's prime status
+     */
     public static boolean isPrime(int a){
         if (a == 1 || a == 0){
             Logs.log(Level.INFO, "Number between 0 and 1 - not prime");
